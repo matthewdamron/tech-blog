2b982d2faf4aff8d4480ae7b4dd6c73329369cf4
Matthew Damron <mattdamron@msn.com>
2021-02-19 13:24:52 -0700

    added comment api

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 const User = require('./User');
 const Post = require('./Post');
 // const Vote = require('./Vote');
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 
 // // create associations
 User.hasMany(Post, {
@@ -47,24 +47,24 @@ Post.belongsTo(User, {
 //   foreignKey: 'post_id'
 // });
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// Comment.belongsTo(Post, {
-//   foreignKey: 'post_id',
-//   onDelete: 'SET NULL'
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// Post.hasMany(Comment, {
-//   foreignKey: 'post_id'
-// });
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 
 // module.exports = { User, Post, Vote, Comment };
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
